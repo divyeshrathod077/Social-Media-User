52e9ace4ae04bd786734deccb661ddd3b0da0f58
Divyesh Rathod <rathoddivyesh5911@gmail.com>
2026-06-03 23:29:00 +0530

    env fix

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,2 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://social-media-server-six-virid.vercel.app/";
 export default BASE_URL;
