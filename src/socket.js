dfc7aa45b9e6071fe6dcdeef5448645b16223a07
Divyesh Rathod <rathoddivyesh5911@gmail.com>
2026-06-05 21:33:23 +0530

    fix socket

--- src/socket.js
+++ src/socket.js
@@ -2,11 +2,11 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "https://social-media-server-eiad.onrender.com"
+    ? "https://social-media-server-eiad.onrender.com" // ✅ your backend Render URL
     : "http://localhost:3001";
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket", "polling"],
+  transports: ["websocket", "polling"], // allow fallback
   withCredentials: true,
 });
 
